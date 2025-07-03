14d112ec3e72178395c819cbef8b8d2b02a9db7e
Sebastian Davidovic <davidovicsebastian@gmail.com>
2025-07-03 20:03:01 +0000

    draw

--- player.py
+++ player.py
@@ -18,6 +18,9 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
     
+    def draw(self, screen):
+        pygame.draw.polygon(screen, (255, 255, 255), self.triangle())
+        
     def update(self, dt):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
