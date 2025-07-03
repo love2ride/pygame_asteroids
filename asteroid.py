c52ebeff52a2d69a4c2d6e8cd14d77e7092a4c19
Sebastian Davidovic <davidovicsebastian@gmail.com>
2025-07-03 21:10:48 +0000

    player groups

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
+import random
 from circleshape import CircleShape
+from constants import ASTEROID_MIN_RADIUS
 
 class Asteroid(CircleShape):
     containers = ()  # Wird im main.py gesetzt
@@ -14,3 +16,19 @@ class Asteroid(CircleShape):
 
     def draw(self, surface):
         pygame.draw.circle(surface, (200, 200, 200), self.position, self.radius, 2)
+    
+    def split(self):
+        # Asteroid zerstören
+        self.kill()
+        # Wenn zu klein, keine weiteren Asteroiden erzeugen
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        # Zwei neue Asteroiden erzeugen
+        random_angle = random.uniform(20, 50)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        v1 = self.velocity.rotate(random_angle) * 1.2
+        v2 = self.velocity.rotate(-random_angle) * 1.2
+        a1 = Asteroid(self.position.x, self.position.y, new_radius)
+        a1.velocity = v1
+        a2 = Asteroid(self.position.x, self.position.y, new_radius)
+        a2.velocity = v2
