e46904154426741bb5f2ae9608c71412f5349128
Sebastian Davidovic <davidovicsebastian@gmail.com>
2025-07-03 20:45:21 +0000

    player groups

--- asteroid.py
+++ asteroid.py
@@ -7,7 +7,7 @@ class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
         for group in self.containers:
-            group.append(self)
+            group.add(self)
 
     def update(self, dt):
         self.position += self.velocity * dt
